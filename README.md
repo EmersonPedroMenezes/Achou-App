3ca7183eda817f0c8592d46a56c74be23402c2e5
Emerson Pedro <emersonpedro.nmenezes@gmail.com>
2023-05-23 00:01:03 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Achou-App
+Projeto Desenvolvido no 5º Período na faculdade de Sistemas de Informação junto ao professor Edemilton na disciplina de Desenvolvimento Mobile.
---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7Kuychk2y0e-8MdCoJhCIHNke9IsJJrc",
+  authDomain: "achou-app-aula-f18b7.firebaseapp.com",
+  projectId: "achou-app-aula-f18b7",
+  storageBucket: "achou-app-aula-f18b7.appspot.com",
+  messagingSenderId: "532337817215",
+  appId: "1:532337817215:web:68e076a06c978104dab14a"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
